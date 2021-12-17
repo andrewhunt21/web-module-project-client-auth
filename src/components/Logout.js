3364234da9a9bbb5c87eba3346c62bb23a1a5826
andrewhunt21 <andrewhunt21@icloud.com>
2021-12-16 17:42:49 -0800

    made code dry with axioswithauth

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,16 +1,12 @@
 import React, {useEffect} from 'react';
 import { useHistory } from 'react-router-dom';
 import axios from 'axios';
+import axiosWithAuth from '../utils/axiosWithAuth';
 
 const Logout = () => {
     const { push } = useHistory();
     useEffect(() => {
-        const token = localStorage.getItem("token");
-        axios.post('http://localhost:9000/api/logout', {}, {
-            headers: {
-                authorization: token
-            }
-        })
+        axiosWithAuth().post('/logout', {})
             .then(resp => {
                 localStorage.removeItem("token");
                 push('/login');
