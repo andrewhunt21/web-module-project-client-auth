daa859831e701984c99285382decc7501e362a60
andrewhunt21 <andrewhunt21@icloud.com>
2021-12-16 17:18:40 -0800

    setup logout

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route, Redirect, Link} from 'react-router-dom'
 import Login from './components/Login';
 import FriendsList from './components/FriendsList';
 import AddFriend from './components/AddFriends';
+import Logout from './components/Logout';
 
 function App() {
   return (
@@ -15,19 +16,27 @@ function App() {
           <Link className="link" to="login">Login</Link>
           <Link className="link" to="friends">Friends List</Link>
           <Link className="link" to="friends/add">Add Friends</Link>
-          <Link className="link" to="">Logout</Link>
+          <Link className="link" to="logout">Logout</Link>
         </header>
+
         <Route exact path="/">
           <Login />
         </Route>
+
         <Route exact path="/login">
           <Redirect to="/" />
         </Route>
+
         <Route exact path="/friends">
           <FriendsList />
         </Route>
+
         <Route exact path="/friends/add">
           <AddFriend />
+        </Route>
+
+        <Route exact path="/logout">
+          <Logout />
         </Route>
       </div>
     </Router>
